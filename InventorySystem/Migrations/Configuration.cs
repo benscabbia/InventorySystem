cff0114a041cdaa0f093e812675302bcc7978095
Benjamin Scabbia <benscabbia@gmail.com>
2017-01-13 07:15:53 +0000

    added seed data

--- InventorySystem/Migrations/Configuration.cs
+++ InventorySystem/Migrations/Configuration.cs
@@ -16,24 +16,10 @@ namespace InventorySystem.Migrations
 
         protected override void Seed(InventorySystem.Models.InventorySystemDb context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
-
             context.Items.AddOrUpdate(
                 i => i.Name,
                      new Item
-                     {
-                         Id = 1,
+                     {                     
                          Name = "Yoda",
                          Description = "",
                          Price = 8,
@@ -42,14 +28,49 @@ namespace InventorySystem.Migrations
                          BoxId = 1
                      },
                     new Item
-                    {
-                        Id = 2,
+                    {                        
                         Name = "Bart Simpson",
                         Description = "Small canvas",
                         Price = 3,
                         Category = Categories.TV,
                         EbayUrl = "www.sims.com",
-                        BoxId = 4
+                        BoxId = 2
+                    }
+             );
+
+            context.Boxes.AddOrUpdate(
+                b => b.Label,
+                    new Box
+                    {
+                        Label = "Disney Soft Toys",
+                        Fullness = 0,
+                        Category = Categories.Disney,
+                        Capacity = 65,
+                        Value = 0
+                    },
+                    new Box
+                    {
+                        Label = "Disney Hard Toys",
+                        Fullness = 0,
+                        Category = Categories.Disney,
+                        Capacity = 65,
+                        Value = 0
+                    },
+                    new Box
+                    {
+                        Label = "Film and TV Soft Toys",
+                        Fullness = 0,
+                        Category = Categories.TV,
+                        Capacity = 65,
+                        Value = 0
+                    },
+                    new Box
+                    {
+                        Label = "Film and TV Hard Toys",
+                        Fullness = 0,
+                        Category = Categories.TV,
+                        Capacity = 65,
+                        Value = 0
                     }
              );
         }
