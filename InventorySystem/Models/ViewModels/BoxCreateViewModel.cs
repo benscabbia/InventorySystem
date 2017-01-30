62e8fe97d9b2ebeda4f24a6d5084dcf29e182d3c
Benjamin Scabbia <benscabbia@gmail.com>
2017-01-30 07:09:21 +0000

    Added BoxCreate and BoxEdit view models

---
+++ InventorySystem/Models/ViewModels/BoxCreateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace InventorySystem.Models.ViewModels
+{
+    public class BoxCreateViewModel
+    {
+        [Required]
+        public string Label { get; set; }
+        public int Capacity { get; set; }
+        public int CategoryId { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
---
+++ InventorySystem/Models/ViewModels/BoxEditViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace InventorySystem.Models.ViewModels
+{
+    public class BoxEditViewModel
+    {
+        public int Id { get; set; }
+        public string Label { get; set; }
+        public int Capacity { get; set; }
+        public int CategoryId { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+
+    }
+}
